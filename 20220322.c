f22c9675a08262f9ce332ae1c1bb2e4043ee517f
kim-guenyoung <101184338+kim-guenyoung@users.noreply.github.com>
2022-03-22 23:21:28 +0900

    Add files via upload

---
+++ 20220322.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+int main(void)
+{
+	printf("Let me introduce myself\n");
+	printf("My age : %d\n", 20);
+	printf("%d is my point\n", 100);
+	printf("Good\nMorning\nEveryone!");
+	return 0;
+}*/
+
+#include <stdio.h>
+int main(void)
+{
+	printf("이름 : %s\n", "김근영");
+	printf("주소 : %s\n", "허리도 가늘군 만지면 부러지리");
+	printf("전화번호 : 010-2304-3165");
+	return 0;
+}
